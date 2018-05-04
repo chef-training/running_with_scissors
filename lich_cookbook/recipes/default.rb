33ff29e5d331e54f543e12a215464bf3f047e265
Franklin Webber <franklin.webber@gmail.com>
2018-05-04 15:24:59 -0500

    Updates lich cookbook to complete deployment
    Signed-off-by: Franklin Webber <franklin@chef.io>

--- lich_cookbook/recipes/default.rb
+++ lich_cookbook/recipes/default.rb
@@ -14,14 +14,17 @@ package 'unzip'
 
 cookbook_file '/srv/lich.zip' do
   source 'lich.zip'
+  notifies :run, 'execute[extract_site]', :immediately
 end
 
 execute 'extract_site' do
   command 'unzip lich.zip'
   cwd '/srv'
+  action :nothing
 end
 
 application '/srv/lich' do
+  environment({ "PORT" => 8000 })
   npm_install
   npm_start
 end
--- lich_cookbook/test/integration/default/default_test.rb
+++ lich_cookbook/test/integration/default/default_test.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
 # https://inspec.io/docs/reference/resources/http/
-describe http('http://localhost:8000') do
+describe http('localhost:8000', enable_remote_worker: true) do
   its('status') { should eq 200 }
 end
